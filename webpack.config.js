a2eef23684313f7bd224bfc02645da435397ebb8
Alireza Razinejad <a.razinejad@gmail.com>
2021-08-17 21:44:30 +0430

    Update webpack.config
    Implemented solution to run dev server and serve the app

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 module.exports = {
+    mode: 'development',
     entry: './src/index.js',
     output: {
         filename: "main.js",
@@ -17,5 +18,10 @@ module.exports = {
                 ]
             }
         ]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 4200
     }
 }
